28c92f045bfbf06475b3d3835e8c5cb032ebf072
Đào Bá Khánh <daobakhanhbk@gmail.com>
2022-05-22 23:00:16 +0700

    Initial App

--- lib/main.dart
+++ lib/main.dart
@@ -48,160 +48,21 @@ class MyHomePage extends StatelessWidget {
     final widthQuarterOfScreen = (size.width - marginCard) / 4;
 
     return Scaffold(
-      body: SafeArea(
-        // top: true,
-        child: Container(
-          color: Colors.white,
-          child: Stack(
-            // alignment: Alignment.center,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: heightSmallCardCustom,
-                          color: Colors.blue,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                  flex: 1,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        flex: 1,
-                                        child: Container(
-                                          // height: heightSmallCardCustom / 4,
-                                          // width: widthQuarterOfScreen / 3,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Container(
-                                          // height: heightSmallCardCustom / 4,
-                                          // width: widthQuarterOfScreen / 3,
-                                          color: Colors.orange,
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Container(
-                                          // height: heightSmallCardCustom / 4,
-                                          // width: widthQuarterOfScreen / 3,
-                                          color: Colors.blueGrey,
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Container(
-                                          // height: heightSmallCardCustom / 4,
-                                          // width: widthQuarterOfScreen / 3,
-                                          color: Colors.red,
-                                        ),
-                                      )
-                                    ],
-                                  )),
-                              Expanded(
-                                flex: 2,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(
-                                            // width: widthQuarterOfScreen / 3,
-                                            height: heightSmallCardCustom / 4,
-                                            color: Colors.green,
-                                          ),
-                                        ),
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(
-                                            // width: widthQuarterOfScreen / 3,
-                                            height: heightSmallCardCustom / 4,
-                                            color: Colors.yellow,
-                                          ),
-                                        )
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        Container(
-                          height: heightSmallCardDefault,
-                          color: Colors.black,
-                        ),
-                        Container(
-                          height: heightSmallCardDefault,
-                          color: Colors.yellowAccent,
-                        )
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      color: Colors.pink,
-                      // width: size.width / 2,
-                      margin: const EdgeInsets.only(right: marginCard),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.pink,
-                    ),
-                  ),
-                ],
-              ),
-              Positioned(
-                  // left: 0,
-                  // top: 0,
-                  right: 20,
-                  bottom: 20,
-                  child: Container(
-                      child: IconButton(
-                        icon: const Icon(Icons.home),
-                        color: Colors.white,
-                        onPressed: () {},
-                      ),
-                      height: 50,
-                      width: 50,
-                      decoration: const BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black38,
-                                offset: Offset(3, 6),
-                                blurRadius: 6)
-                          ],
-                          color: Colors.blue,
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(24))))),
-              Positioned(
-                  bottom: heightSmallCardDefault * 4 / 3,
-                  left: (widthQuarterOfScreen * 2) / 3,
-                  child: Container(
-                    width: widthQuarterOfScreen,
-                    height: heightSmallCardDefault / 2,
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.5),
-                    ),
-                  ))
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        title: const Text('All String'),
+      ),
+      body: Container(
+        color: Colors.white,
       ),
     );
+  }
+}
+
+class CardScreen extends StatelessWidget {
+  const CardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
